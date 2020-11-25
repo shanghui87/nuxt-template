a628ab8016303c3b92d82fb586cacf22b364dd1a
shanghui87 <71508837+shanghui87@users.noreply.github.com>
2020-11-25 21:29:31 +0800

    My First Website

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <div>
-    <div class='mt-20'>
-      <Logo style='width:120px;margin:auto' />
-      <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
-      </h1>
-      <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
-      </h2>
-      
+  <div class="bg-grayish-white-1 relative">
+    <div class="h-screen w-2/3">
+      <img
+        class="w-full h-full object-cover"
+        src="https://images.pexels.com/photos/331684/pexels-photo-331684.jpeg?cs=srgb&dl=pexels-math-331684.jpg&fm=jpg"
+      />
+    </div>
+
+    <div class="bg-red-400">
+      <!-- Display Name-->
+      <div class="absolute h-screen w-1/2 top-0 right-0">
+        <div class="flex h-full w-full">
+          <div class="my-auto w-full">
+            <div class="flex flex-col">
+              <div class="text-4xl font-poppins text-gray-800">
+                <span class="font-thin">Welcome to</span>
+                <span class="font-semibold">Tie's Page</span>
+                <span class="text-6xl text-lead-yellow">.</span>
+              </div>
+            </div>
+            <div class="font-sans text-gray-700 text-center -mt-2">
+              Founder of Gruda Tech | Instructor @ LEAD | Data Scientist |
+              FullStack Developer
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
